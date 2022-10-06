c775299f2fb76d1b9afeb65198bd9eeddc45c86b
schamling <schamling@deloitte.com>
2022-10-06 17:13:52 +0000

    some message

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Suresh';
+    team = 'CHAMLING';
     version = '1.0.0';
 }
 module.exports = AppConfig;
