596e264bffbe73618a2c6be6255c1b08331c12d4
schamling <schamling@deloitte.com>
2022-10-06 17:02:04 +0000

    My first change

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Suresh';
+    team = 'Chamling';
     version = '1.0.0';
 }
 module.exports = AppConfig;
